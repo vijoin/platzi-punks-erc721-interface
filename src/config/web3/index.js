ddc6f942e81f6d0a8d7151e916c8099c803fe4a1
Victor Inojosa <vijoin@gmail.com>
2022-01-17 09:11:32 -0300

    Fix minor errors

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,12 +1,14 @@
 import Web3 from 'web3';
-import { InjectedConnector } from '@web-react/injected-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
 
-const connector = new InjectedConnector({ supportdChainIds: [
-    4 // Rinkeby
-] })
+const connector = new InjectedConnector({ 
+    supportdChainIds: [
+        4, // Rinkeby
+    ],
+});
 
-function getLibrary(provider) {
-    return new Web3(provider)
-}
+const getLibrary = (provider) => {
+    return new Web3(provider);
+};
 
-export { getLibrary, connector };
+export { connector, getLibrary };
