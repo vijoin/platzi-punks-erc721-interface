5a65012251894c92099b04efe72eebe3408a1c6d
Victor Inojosa <vijoin@gmail.com>
2022-01-30 12:05:26 -0300

    Show all platzi punks in a Gallery

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route } from "react-router-dom";
 import Home from "./views/home";
 import MainLayout from "./layouts/main";
+import Punks from "./views/punks";
 
 function App() {
   return (
     <MainLayout>
       <Route path="/" exact component={Home} />
+      <Route path="/punks" component={Punks}></Route>
     </MainLayout>
   );
 }
 
-export default App;
+export default App; 
