91b975178f9399ad03940bd11f73124b01aaf2d6
Victor Inojosa <vijoin@gmail.com>
2022-02-05 12:24:56 -0300

    Allow transfer a Platzi Punk

--- src/views/punk/index.js
+++ src/views/punk/index.js
@@ -10,6 +10,7 @@ import {
     Tbody,
     Button,
     Tag,
+    useToast,
   } from "@chakra-ui/react";
   import { useWeb3React } from "@web3-react/core";
   import RequestAccess from "../../components/request-access";
@@ -17,72 +18,130 @@ import {
   import { usePlatziPunkData } from "../../hooks/usePlatziPunksData";
   import { useParams } from "react-router-dom";
   import Loading from "../../components/loading";
-  
+  import { useState } from "react";
+  import usePlatziPunks from "../../hooks/usePlatziPunks";
+
   const Punk = () => {
-    debugger;
-    const { active, account } = useWeb3React();
-    const { tokenId } = useParams();
-    const { loading, punk } = usePlatziPunkData(tokenId);
+        const { active, account, library } = useWeb3React();
+        const { tokenId } = useParams();
+        const { loading, punk, update } = usePlatziPunkData(tokenId);
+        const platziPunks = usePlatziPunks();
+        const toast = useToast();
+        const [transfering, setTransfering] = useState(false);
   
-    if (!active) return <RequestAccess />;
+        const transfer = () => {
+          setTransfering(true);
+          
+          const address = prompt("Ingresa la dirección: ");
+          const isAddress = library.utils.isAddress(address);
   
-    if (loading) return <Loading />;
+          if (!isAddress) {
+              toast({
+                  title: "Dirección inválida",
+                  description: "La dirección no es una dirección Ethereum",
+                  status: "error",
   
-    return (
-      <Stack
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 5 }}
-        direction={{ base: "column", md: "row" }}
-      >
-        <Stack>
-          <PunkCard
-            mx={{
-              base: "auto",
-              md: 0,
-            }}
-            name={punk.name}
-            image={punk.image}
-          />
-          <Button disabled={account !== punk.owner} colorScheme="green">
-            {account !== punk.owner ? "No eres el dueño" : "Transferir"}
-          </Button>
-        </Stack>
-        <Stack width="100%" spacing={5}>
-          <Heading>{punk.name}</Heading>
-          <Text fontSize="xl">{punk.description}</Text>
-          <Text fontWeight={600}>
-            DNA:
-            <Tag ml={2} colorScheme="green">
-              {punk.dna}
-            </Tag>
-          </Text>
-          <Text fontWeight={600}>
-            Owner:
-            <Tag ml={2} colorScheme="green">
-              {punk.owner}
-            </Tag>
-          </Text>
-          <Table size="sm" variant="simple">
-            <Thead>
-              <Tr>
-                <Th>Atributo</Th>
-                <Th>Valor</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {Object.entries(punk.attributes).map(([key, value]) => (
-                <Tr key={key}>
-                  <Td>{key}</Td>
-                  <Td>
-                    <Tag>{value}</Tag>
-                  </Td>
+              });
+              setTransfering(false);
+          } else {
+              platziPunks.methods.safeTransferFrom(punk.owner, address, punk.tokenId).send({
+                  from: account,
+              })
+              .on("error", (error) => {
+                  setTransfering(false);
+                  toast({
+                    title: "Transacción fallida",
+                    description: error.message,
+                    status: "error",
+                });
+              })
+              .on("transactionHash", (txHash) => {
+                  toast({
+                      title: "Transaccion Enviada",
+                      description: txHash,
+                      status: "info",
+                  });
+              })
+              .on("receipt", () => {
+                  setTransfering(false);
+                  toast({
+                      title: "Transaccion Confirmada",
+                      description: `El punk ahora pertence a ${address}`,
+                      status: "success",
+                  });
+              });
+              update();
+  
+          };
+  
+          setTransfering(false);
+  
+      };
+    
+      if (!active) return <RequestAccess />;
+    
+      if (loading) return <Loading />;
+    
+      return (
+        <Stack
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 5 }}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Stack>
+            <PunkCard
+              mx={{
+                base: "auto",
+                md: 0,
+              }}
+              name={punk.name}
+              image={punk.image}
+            />
+            <Button
+            onClick={transfer}
+            disabled={account !== punk.owner} 
+            colorScheme="green"
+            isLoading={transfering}
+            >
+              {account !== punk.owner ? "No eres el dueño" : "Transferir"}
+            </Button>
+          </Stack>
+          <Stack width="100%" spacing={5}>
+            <Heading>{punk.name}</Heading>
+            <Text fontSize="xl">{punk.description}</Text>
+            <Text fontWeight={600}>
+              DNA:
+              <Tag ml={2} colorScheme="green">
+                {punk.dna}
+              </Tag>
+            </Text>
+            <Text fontWeight={600}>
+              Owner:
+              <Tag ml={2} colorScheme="green">
+                {punk.owner}
+              </Tag>
+            </Text>
+            <Table size="sm" variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Atributo</Th>
+                  <Th>Valor</Th>
                 </Tr>
-              ))}
-            </Tbody>
-          </Table>
+              </Thead>
+              <Tbody>
+                {Object.entries(punk.attributes).map(([key, value]) => (
+                  <Tr key={key}>
+                    <Td>{key}</Td>
+                    <Td>
+                      <Tag>{value}</Tag>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Stack>
         </Stack>
-      </Stack>
-    );
-  };
-  
-  export default Punk;
+      );
+    };
+    
+    export default Punk;
